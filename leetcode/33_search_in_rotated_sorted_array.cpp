86fc4250e87d1306e0a0e4d9a0cc40f762f0dda2
AsHim Shrestha <ashimsth89@gmail.com>
2024-12-02 08:23:36 +0545

    feat: add binary search code for searching in a rotated sorted array

---
+++ leetcode/33_search_in_rotated_sorted_array.cpp
@@ -0,0 +1,66 @@
+// Search in Rotated Sorted Array Using Binary Search
+
+// Time Complexity: O(logn)
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int search(vector<int> &nums, int target)
+{
+
+    int n = nums.size();
+
+    int low = 0;
+    int high = n - 1;
+
+    while (low <= high)
+    {
+
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] == target)
+        {
+            return mid;
+        }
+
+        if (nums[low] <= nums[mid])
+        {
+
+            if (nums[low] <= target && target < nums[mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        else
+        {
+
+            if (nums[mid] < target && target <= nums[high])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+
+    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+
+    int target = 0;
+
+    cout << search(nums, target);
+
+    return 0;
+}
