813fe7d8b55798ac7b5f79fe0938f02490861169
AsHim Shrestha <ashimsth89@gmail.com>
2025-02-05 22:30:51 +0545

    feat: add linear search implementation

---
+++ labs/lab7/Q2.cpp
@@ -0,0 +1,37 @@
+// Linear Search
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, arr[10];
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    cout << "Enter the elements: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int key;
+
+    cout << "Enter the element to search: ";
+
+    cin >> key;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == key)
+        {
+            cout << "Element found at index " << i << endl;
+            return 0;
+        }
+    }
+
+    cout << "Element not found." << endl;
+
+    return 0;
+}
