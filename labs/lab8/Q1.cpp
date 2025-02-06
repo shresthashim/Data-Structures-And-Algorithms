6f45224dab2db2d4a4642d1039a6933e89f9a1e6
AsHim Shrestha <ashimsth89@gmail.com>
2025-02-06 18:52:47 +0545

    feat: add selection sort implementation

---
+++ labs/lab8/Q1.cpp
@@ -0,0 +1,41 @@
+// Selection Insert
+
+// Time complexity: O(n^2)
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, arr[10];
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
