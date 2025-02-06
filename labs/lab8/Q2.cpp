2480ec9d867990ac81be9496b9ec4e18fafd78ab
AsHim Shrestha <ashimsth89@gmail.com>
2025-02-06 19:10:54 +0545

    feat: add insertion sort implementation

---
+++ labs/lab8/Q2.cpp
@@ -0,0 +1,42 @@
+// Insertion Sort
+
+// Time complexity: O(n^2)
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, arr[10];
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = key;
+    }
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
